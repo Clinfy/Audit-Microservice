d6481583fd8a9b352aea2103cdca72103f9d0edd
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2025-10-14 15:04:42 -0300

    Integrate `@nestjs/config`, `@nestjs/typeorm`, and `@nestjs/microservices` for global config, database, and RabbitMQ setup; add `AuditModule` and `AuditLogsEntity`.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,52 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {AuditModule} from 'src/services/audit/audit.module';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {AuditLogsEntity} from "src/entities/audit-logs.entity";
+import {ClientsModule, Transport} from "@nestjs/microservices";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+imports: [
+    ConfigModule.forRoot({
+        isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+            type: 'postgres',
+            url: configService.get<string>('DATABASE_URL'),
+            entities: [AuditLogsEntity],
+            synchronize: true,
+        }),
+    }),
+
+    ClientsModule.registerAsync([
+        {
+            name: 'AUDIT_LISTENER_SERVICE',
+            imports:[ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                transport: Transport.RMQ,
+                options: {
+                    urls: [configService.get<string>('RABBITMQ_URL') as string],
+                    queue: 'audit_queue',
+                    queueOptions: {
+                        durable: true
+                    },
+                    noAck: false,
+                }
+            })
+        }
+    ]),
+
+    TypeOrmModule.forFeature([AuditLogsEntity]),
+    AuditModule
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
