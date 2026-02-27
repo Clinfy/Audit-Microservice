ea14f6bdde4007da655a807d6def84f7350cb599
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-02-27 16:09:39 -0300

    Add `env-validation` utility for validating environment variables using `class-validator`

---
+++ src/config/env-validation.ts
@@ -0,0 +1,36 @@
+import { IsNotEmpty, IsNumber, IsString, validateSync } from 'class-validator';
+import { plainToInstance } from 'class-transformer';
+
+class EnvironmentVariables {
+  @IsString()
+  @IsNotEmpty()
+  DATABASE_URL: string;
+
+  @IsString()
+  @IsNotEmpty()
+  RABBITMQ_URL: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  PORT: number;
+
+  @IsString()
+  @IsNotEmpty()
+  AUTH_SERVICE_URL: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
