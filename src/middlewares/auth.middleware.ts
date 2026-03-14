1ff38b8b5e4a404ba624121951cb4a350ebf822d
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-14 02:31:35 -0300

    Refactor AuthGuard to use ConfigService for API key and update token extraction method

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,59 +1,60 @@
-import {CanActivate, ExecutionContext, Injectable} from "@nestjs/common";
-import {Reflector} from "@nestjs/core";
-import {extractBearerToken} from "src/common/tools/extract-bearer-auth";
-import axios, {isAxiosError} from "axios";
-import {propagateAxiosError} from "src/common/tools/propagate-axios-error";
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import axios, { isAxiosError } from 'axios';
+import { propagateAxiosError } from 'src/common/tools/propagate-axios-error';
 import { Permissions } from 'src/middlewares/decorators/permission.decorator';
 import { getClientIp } from 'src/common/tools/get-client-ip';
+import { ConfigService } from '@nestjs/config';
+import { extractAuthToken } from 'src/common/tools/extract-bearer-auth';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-    constructor(private readonly reflector: Reflector) {}
+  constructor(private readonly reflector: Reflector, private readonly configService: ConfigService) {}
 
-    async canActivate(context: ExecutionContext): Promise<boolean> {
-        try {
-            const request = context.switchToHttp().getRequest();
-            const token = extractBearerToken(request);
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      const request = context.switchToHttp().getRequest();
+      const token = extractAuthToken(request);
 
-            const requiredPermissions = this.reflector.getAllAndOverride<string[]>(Permissions, [
-                context.getHandler(),
-                context.getClass(),
-            ]);
+      const requiredPermissions = this.reflector.getAllAndOverride<string[]>(
+        Permissions,
+        [context.getHandler(), context.getClass()],
+      );
 
+      if (!requiredPermissions || requiredPermissions.length === 0) return true;
 
-            if (!requiredPermissions || requiredPermissions.length === 0) return true;
+      const baseUrl = process.env.AUTH_SERVICE_URL;
 
+      const requestAuth = requiredPermissions.map((permission: string) =>
+        axios.get(`${baseUrl}/users/can-do/${permission}`, {
+          headers: {
+            'x-api-key': this.configService.get<string>('AUTH_SERVICE_API_KEY'),
+            Authorization: `Bearer ${token}`,
+            'content-type': 'application/json',
+            'x-forwarded-for': getClientIp(request),
+            'x-real-ip': getClientIp(request),
+            'user-agent': request.headers['user-agent'] || '',
+          },
+        }),
+      );
 
-            const baseUrl = process.env.AUTH_SERVICE_URL;
+      const result = await Promise.allSettled(requestAuth);
 
-            const requestAuth = requiredPermissions.map((permission: string ) =>
-                axios.get(`${baseUrl}/users/can-do/${permission}`, {
-                    headers: {
-                        'Authorization': `Bearer ${token}`,
-                        'content-type': 'application/json',
-                        'x-forwarded-for': getClientIp(request),
-                        'x-real-ip': getClientIp(request),
-                        'user-agent': request.headers['user-agent'] || ''
-                    },
-                }),
-            );
+      const atLeastOneAllowed = result.some(
+        (result) => result.status === 'fulfilled' && result.value.data,
+      );
 
-            const result = await Promise.allSettled(requestAuth);
+      if (atLeastOneAllowed) return true;
 
-            const atLeastOneAllowed = result.some(
-                (result) => result.status === 'fulfilled' && result.value.data
-            );
+      const rejected = result.filter(
+        (r): r is PromiseRejectedResult => r.status === 'rejected',
+      );
+      const axiosReject = rejected.find((r) => isAxiosError(r.reason));
+      if (axiosReject) propagateAxiosError(axiosReject.reason);
 
-            if (atLeastOneAllowed) return true;
-
-            const rejected = result.filter((r): r is PromiseRejectedResult => r.status === 'rejected');
-            const axiosReject = rejected.find(r => isAxiosError(r.reason));
-            if (axiosReject) propagateAxiosError(axiosReject.reason);
-
-            return false;
-
-        } catch (error) {
-            throw error;
-        }
+      return false;
+    } catch (error) {
+      throw error;
     }
+  }
 }
