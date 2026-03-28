ff6689b6403eac46f92e4f48dec12031f3dbd845
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-28 16:02:26 -0300

    Refactor AuthGuard to utilize `AuthClientService` for permission checks and remove direct Axios calls

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,15 +1,12 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import axios, { isAxiosError } from 'axios';
-import { propagateAxiosError } from 'src/common/tools/propagate-axios-error';
 import { Permissions } from 'src/middlewares/decorators/permission.decorator';
-import { getClientIp } from 'src/common/tools/get-client-ip';
-import { ConfigService } from '@nestjs/config';
 import { extractAuthToken } from 'src/common/tools/extract-bearer-auth';
+import { AuthClientService } from 'src/clients/auth/auth-client.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector, private readonly configService: ConfigService) {}
+  constructor(private readonly reflector: Reflector, private readonly authClient: AuthClientService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
@@ -23,36 +20,14 @@ export class AuthGuard implements CanActivate {
 
       if (!requiredPermissions || requiredPermissions.length === 0) return true;
 
-      const baseUrl = process.env.AUTH_SERVICE_URL;
-
-      const requestAuth = requiredPermissions.map((permission: string) =>
-        axios.get(`${baseUrl}/users/can-do/${permission}`, {
-          headers: {
-            'x-api-key': this.configService.get<string>('AUTH_SERVICE_API_KEY'),
-            Authorization: `Bearer ${token}`,
-            'content-type': 'application/json',
-            'x-forwarded-for': getClientIp(request),
-            'x-real-ip': getClientIp(request),
-            'user-agent': request.headers['user-agent'] || '',
-          },
-        }),
+      const userCanDo = await Promise.all(
+        requiredPermissions.map((permission: string) =>
+          this.authClient.canDo(permission, token, request),
+        ),
       );
 
-      const result = await Promise.allSettled(requestAuth);
+      return userCanDo.some(Boolean);
 
-      const atLeastOneAllowed = result.some(
-        (result) => result.status === 'fulfilled' && result.value.data,
-      );
-
-      if (atLeastOneAllowed) return true;
-
-      const rejected = result.filter(
-        (r): r is PromiseRejectedResult => r.status === 'rejected',
-      );
-      const axiosReject = rejected.find((r) => isAxiosError(r.reason));
-      if (axiosReject) propagateAxiosError(axiosReject.reason);
-
-      return false;
     } catch (error) {
       throw error;
     }
