36fce050f4b87d6d64d1ed666bf26201e73b3237
Agustin Liendo Ortiz <agusliendo2004@gmail.com>
2026-03-28 21:10:40 -0300

    Replace `Permissions` decorator with `EndpointKey` decorator for metadata resolution in `AuthGuard` and `AuditController`. Refactor permission handling to utilize endpoint-based permissions.

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,40 +1,39 @@
 import {
   CanActivate,
   ExecutionContext,
-  Injectable, InternalServerErrorException,
+  Injectable,
+  InternalServerErrorException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Permissions } from 'src/common/decorators/permission.decorator';
 import { extractAuthToken } from 'src/common/utils/extract-bearer-auth.util';
 import { AuthClientService } from 'src/clients/auth/auth-client.service';
+import { EndpointKey } from 'src/common/decorators/endpoint-key.decorator';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector, private readonly authClient: AuthClientService) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly authClient: AuthClientService,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
       const request = context.switchToHttp().getRequest();
       const token = extractAuthToken(request);
 
-      const requiredPermissions = this.reflector.getAllAndOverride<string[]>(
-        Permissions,
-        [context.getHandler(), context.getClass()],
-      );
+      const endpointKey = this.reflector.getAllAndOverride<string>(EndpointKey, [context.getHandler(), context.getClass()]);
+      const requiredPermissions = await this.authClient.getEndpointPermissions(endpointKey, request);
 
       if (!requiredPermissions || requiredPermissions.length === 0) return true;
 
       const userCanDo = await Promise.all(
-        requiredPermissions.map((permission: string) =>
-          this.authClient.canDo(permission, token, request),
-        ),
+        requiredPermissions.map((permission: string) => this.authClient.canDo(permission, token, request)),
       );
 
-      if(!userCanDo.some(Boolean)) throw new UnauthorizedException()
+      if (!userCanDo.some(Boolean)) throw new UnauthorizedException();
 
       return userCanDo.some(Boolean);
-
     } catch (error) {
       throw new InternalServerErrorException(error);
     }
--- src/services/audit/audit.controller.ts
+++ src/services/audit/audit.controller.ts
@@ -1,42 +1,40 @@
-import {Controller, Get, Param, UseGuards} from '@nestjs/common';
-import {AuditService} from 'src/services/audit/audit.service';
-import {Ctx, EventPattern, Payload, RmqContext} from '@nestjs/microservices';
-import {AuditLogsEntity} from 'src/entities/audit-logs.entity';
-import {AuthGuard} from "src/common/guards/auth.guard";
-import {Permissions} from "src/common/decorators/permission.decorator";
-
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { AuditService } from 'src/services/audit/audit.service';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { AuditLogsEntity } from 'src/entities/audit-logs.entity';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { EndpointKey } from 'src/common/decorators/endpoint-key.decorator';
 
 @Controller('audit')
 export class AuditController {
-    constructor(private readonly auditService: AuditService) {}
+  constructor(private readonly auditService: AuditService) {}
 
-    @EventPattern('#')
-    async handleAuditEvent(@Payload() data: any, @Ctx() context: RmqContext) {
-        const channel = context.getChannelRef();
-        const originalMsg = context.getMessage();
-        const pattern = context.getPattern();
-        try {
-            await this.auditService.recordEvent(pattern ?? 'unknown', data);
+  @EventPattern('#')
+  async handleAuditEvent(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    const pattern = context.getPattern();
+    try {
+      await this.auditService.recordEvent(pattern ?? 'unknown', data);
 
-            channel.ack(originalMsg);
-
-        } catch (error) {
-            console.error('Error handling audit event:', error);
-            channel.nack(originalMsg, false, false);
-        }
+      channel.ack(originalMsg);
+    } catch (error) {
+      console.error('Error handling audit event:', error);
+      channel.nack(originalMsg, false, false);
     }
+  }
 
-    @UseGuards(AuthGuard)
-    @Permissions(['LOGGER_READ'])
-    @Get('all')
-    getAllLogs(): Promise<AuditLogsEntity[]> {
-        return this.auditService.getAllLogs();
-    }
+  @UseGuards(AuthGuard)
+  @EndpointKey('logger.find')
+  @Get('all')
+  getAllLogs(): Promise<AuditLogsEntity[]> {
+    return this.auditService.getAllLogs();
+  }
 
-    @UseGuards(AuthGuard)
-    @Permissions(['LOGGER_READ'])
-    @Get('pattern/:pattern')
-    getLogsByPattern(@Param('pattern') pattern: string): Promise<AuditLogsEntity[]> {
-        return this.auditService.getLogsByPattern(pattern);
-    }
+  @UseGuards(AuthGuard)
+  @EndpointKey('logger.find')
+  @Get('pattern/:pattern')
+  getLogsByPattern(@Param('pattern') pattern: string): Promise<AuditLogsEntity[]> {
+    return this.auditService.getLogsByPattern(pattern);
+  }
 }
